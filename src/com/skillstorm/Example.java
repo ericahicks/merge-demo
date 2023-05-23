4ffde7004193d6f9e1065bdd70482c127c899610
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-23 16:50:15 -0400

    whoops committing to main

--- src/com/skillstorm/Example.java
+++ src/com/skillstorm/Example.java
@@ -4,6 +4,13 @@ public class Example {
 
 	public static void main(String[] args) {
 		System.out.println("this is my code");
+		multithreading();
+	}
+	
+	public static void multithreading() { // no guaranteed order 
+		new Thread(() -> System.out.println("thread 1")).start();
+		new Thread(() -> System.out.println("thread 2")).start();
+		System.out.println("multithreading");
 	}
 
 }
