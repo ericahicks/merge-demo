4f99376c96cb6b2326b01d9018c24e20c46d08bb
ericahicks <48265629+ericahicks@users.noreply.github.com>
2023-05-23 16:22:17 -0400

    more print statments

--- src/com/skillstorm/Example.java
+++ src/com/skillstorm/Example.java
@@ -2,8 +2,10 @@ package com.skillstorm;
 
 public class Example {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("this is my code");
+		Thread.sleep(2000);
+		System.out.println("please do not overwrite me");
 	}
 
 }
